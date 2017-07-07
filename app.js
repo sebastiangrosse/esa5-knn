3482640e8892ad573e8c2af5c2b4683aee6213c2
Sebastian Große <sebastian.grosse91@gmail.com>
2017-07-07 16:10:46 +0200

    trial logs input and data

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const kNN = require('./kNN')
 fs.readFile('./kicks_kisses_data.json', 'utf8', (err, kicks_kisses) => {
     if (err) throw err;
     let data = JSON.parse(kicks_kisses);
+    console.log('Data:',data);
+    let input = [18, 90];
+    console.log('Input:',input)
     result = kNN.classify([18, 90], data, 3);
-    console.log(result);
+    console.log('Result:',result);
 })
